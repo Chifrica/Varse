9af75a8e55638ae04d9c5d57b7d4e47471009af1
Onwunali Blessing Chika <chikaonwunali2019@gmail.com>
2025-10-14 09:58:21 +0100

    imported icons libraries

--- app/src/(root)/(tab)/_layout.tsx
+++ app/src/(root)/(tab)/_layout.tsx
@@ -1,118 +1,54 @@
-// import { Tabs } from "expo-router";
-// import React from "react";
-// import { Image, ImageBackground, StyleSheet, View } from "react-native";
-// import icons from "../../../constants/icons";
-// import images from "../../../constants/images";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import React from "react";
 
-// const TabIcon = ({ focused, icon }: any) => {
-// 	if (focused) {
-// 		return (
-// 			<>
-// 				<ImageBackground
-// 					source={images.background}
-// 					style={styles.container}>
-// 					<Image
-// 						source={icon}
-// 						style={{
-// 							width: 18,
-// 							height: 18,
-// 							top: "25%",
-// 							right: "35%",
-// 							position: "absolute",
-// 						}}
-// 					/>
-// 				</ImageBackground>
-// 			</>
-// 		);
-// 	}
+const AppLayout = () => {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "#FF8800",
+                tabBarInactiveTintColor: "#999",
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
+                name="homePage/home"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="home" size={22} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="category/category"
+                options={{
+                    title: "Categories",
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="th-list" size={22} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="cart/cart"
+                options={{
+                    title: "Cart",
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="shopping-cart" size={22} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile/profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="user" size={22} color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+};
 
-// 	return (
-// 		<View>
-// 			<Image
-// 				source={icon}
-// 				style={{
-// 					width: 20,
-// 					height: 20,
-// 				}}
-// 				tintColor={"#E58945"}
-// 			/>
-// 		</View>
-// 	);
-// };
-// const AppLayout = () => {
-// 	return (
-// 		<Tabs
-// 			screenOptions={{
-// 				tabBarLabelStyle: { color: "#E58945" },
-// 			}}>
-// 			<Tabs.Screen
-// 				name="home/homePage"
-// 				options={{
-// 					title: "Home",
-// 					headerShown: false,
-// 					tabBarIcon: ({ focused }) => (
-// 						<TabIcon
-// 							focused={focused}
-// 							icon={icons.home}
-// 						/>
-// 					),
-// 				}}
-// 			/>
-// 			<Tabs.Screen
-// 				name="category/categoryScreen"
-// 				options={{
-// 					title: "Categories",
-// 					headerShown: false,
-// 					tabBarIcon: ({ focused }) => (
-// 						<TabIcon
-// 							focused={focused}
-// 							icon={icons.explore}
-// 						/>
-// 					),
-// 				}}
-// 			/>
-// 			<Tabs.Screen
-// 				name="cart/cartScreen"
-// 				options={{
-// 					title: "Cart",
-// 					headerShown: false,
-// 					tabBarIcon: ({ focused }) => (
-// 						<TabIcon
-// 							focused={focused}
-// 							icon={icons.order}
-// 						/>
-// 					),
-// 				}}
-// 			/>
-// 			<Tabs.Screen
-// 				name="profile/profileScreen"
-// 				options={{
-// 					title: "Profile",
-// 					headerShown: false,
-// 					tabBarIcon: ({ focused }) => (
-// 						<TabIcon
-// 							focused={focused}
-// 							icon={icons.schedule}
-// 						/>
-// 					),
-// 				}}
-// 			/>
-// 		</Tabs>
-// 	);
-// };
-
-// export default AppLayout;
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		position: "relative",
-// 		display: "flex",
-// 		width: "100%",
-// 		height: "100%",
-// 		minWidth: 60,
-// 		minHeight: 60,
-// 		borderRadius: 50,
-// 		marginBottom: 20,
-// 		overflow: "hidden",
-// 	},
-// });
+export default AppLayout;
--- app/src/(root)/(tab)/homePage/home.tsx
+++ app/src/(root)/(tab)/homePage/home.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Image, ScrollView, Text, TextInput, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import styles from './style'
+import Fontawesome from '@expo/vector-icons/FontAwesome';
+import React from 'react';
+import { Image, ScrollView, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
     return (
@@ -15,10 +15,13 @@ const Home = () => {
                     <Text style={styles.headerTitle}>Varse Market</Text>
                     <View style={styles.headerGasCart}>
                         <View style={styles.headerGas}>
-                            <Text>Gas cylinder</Text>
+                            <Image 
+                                style={{ width: 18, height: 18, marginRight: 10, }}
+                                source={{uri: 'https://cdn-icons-png.flaticon.com/128/5771/5771077.png'}}
+                            />
                             <Text>Refill Gas</Text>
                         </View>
-                        <Text>cart</Text>
+                        <Fontawesome name="shopping-cart" size={24} color="black" style={{ marginLeft: 15 }} />
                     </View>
                 </View>
 
@@ -62,10 +65,64 @@ const Home = () => {
                     </View> 
                 </View>
 
-
             </ScrollView>
         </SafeAreaView>
     )
 }
 
 export default Home;
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+		backgroundColor: "#FFFFFF",
+	},
+	scrollView: {
+		flex: 1,
+		paddingHorizontal: 15,
+	},
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        top: 10,
+    },
+    headerTitle: {
+        fontSize: 24,
+        height: 30,
+        fontWeight: "bold",
+    },
+    headerGasCart: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+    headerGas: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderRadius: 50,
+        backgroundColor: "#FF8800",
+        paddingRight: 10,
+        paddingLeft: 10,
+        alignItems: "center",
+        height: 30,
+    },
+    search: {
+        borderRadius: 50,
+    },
+    category: {
+        flexDirection: "row"
+    },
+    categoryProducts: {
+        
+    },
+    categoryProductsImage: {
+        borderRadius: 100
+    },
+    categoryProductsTxt: {
+        fontSize: 18,
+        fontWeight: 600
+    },
+    featuredShops: {},
+    trendingProducts: {},
+})
