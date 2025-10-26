20302cd08756c9349d2b04e30560c87cf6d53741
Onwunali Blessing Chika <chikaonwunali2019@gmail.com>
2025-10-27 00:05:09 +0100

    updated some order screen

--- app/vendor/(root)/(tab)/order/cancelledOrder.tsx
+++ app/vendor/(root)/(tab)/order/cancelledOrder.tsx
@@ -1,17 +1,74 @@
-import React from 'react'
-import { ScrollView, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const CancelledOrder = () => {
-  return (
-    <SafeAreaView>
-        <ScrollView>
-            <View>
 
-            </View>
-        </ScrollView>
-    </SafeAreaView>
-  )
+    const router = useRouter();
+
+    const handleBackArrow = () => {
+        router.back()
+    }
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={handleBackArrow}>
+                        <Ionicons name="arrow-back-outline" size={24} color="#000" />
+                    </TouchableOpacity>
+                    <Text style={styles.title}>Pending Orders</Text>
+                </View>
+
+                <View>
+                    <View>
+                        <View>
+                            <Image />
+                            <View>
+                                <Text>Medium Sized Box</Text>
+                                <Text>Order ID: #VAR123ABC</Text>
+                            </View>
+                            <Text>Pending</Text>
+                            <Ionicons name="chevron-forward-outline" size={20} color="#000" />
+                        </View>
+
+                        <View>
+                            <Image />
+                            <View>
+                                <Text>Medium Sized Box</Text>
+                                <Text>Order ID: #VAR123ABC</Text>
+                            </View>
+                            <Text>Pending</Text>
+                            <Ionicons name="chevron-forward-outline" size={20} color="#000" />
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default CancelledOrder
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "#f9f9f9",
+    },
+    header: {
+        marginBottom: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 16,
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: "700",
+        color: "#000",
+        alignSelf: "center",
+        marginLeft: 100,
+    },
+})
--- app/vendor/(root)/(tab)/order/pendingOrder.tsx
+++ app/vendor/(root)/(tab)/order/pendingOrder.tsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import { ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const PendingOrder = () => {
-  return (
-    <SafeAreaView>
-        <ScrollView>
 
-        </ScrollView>
-    </SafeAreaView>
-  )
+    const router = useRouter();
+
+    const handleBackArrow = () => {
+        router.back();
+    }
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={handleBackArrow}>
+                        <Ionicons name="arrow-back-outline" size={24} color="#000" />
+                    </TouchableOpacity>
+                    <Text style={styles.title}>Pending Orders</Text>
+                </View>
+
+                <View>
+                    <View>
+                        <Image />
+                        <View>
+                            <Text>Medium Sized Box</Text>
+                            <Text>Order ID: #VAR123ABC</Text>
+                        </View>
+                        <Text>Pending</Text>
+                        <Ionicons name="chevron-forward-outline" size={20} color="#000" />
+                    </View>
+
+                    <View>
+                        <Image />
+                        <View>
+                            <Text>Medium Sized Box</Text>
+                            <Text>Order ID: #VAR123ABC</Text>
+                        </View>
+                        <Text>Pending</Text>
+                        <Ionicons name="chevron-forward-outline" size={20} color="#000" />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default PendingOrder
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f9f9f9",
+  },
+  header: {
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: "#000",
+    alignSelf: "center",
+    marginLeft: 100,
+  },
+})
